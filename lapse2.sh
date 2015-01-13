eb834f7fc7ecf7c6f77e13cd17273d73e4bead20
kostavro <kostavro@gmail.com>
2015-01-13 14:05:19 +0000

    fixed

--- lapse2.sh
+++ lapse2.sh
@@ -3,6 +3,6 @@
 # mount sda1 into /media/Tough/ 
 sudo mount /dev/sda1 /media/Tough/
 # images, no preview, x&y inverted, 1h duration, every 3 sec, 4 digits name, saved as jpg
-raspistill -bm -hf -vf -t 30000 -tl 3000 -o /media/Tough/lapse/image%04d.jpg
+raspistill -bm -hf -vf -t 3600000 -tl 3000 -o /media/Tough/lapse/image%04d.jpg
 # unmount sda1
 sudo umount /dev/sda1
