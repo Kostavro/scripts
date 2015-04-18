4dc615481dc3c93ec011e7d8210d744856a61f64
kostavro <kostavro@gmail.com>
2015-04-19 02:24:49 +0300

    fixed lapse. will test later

--- lapse2.sh
+++ lapse2.sh
@@ -2,7 +2,12 @@
 
 # mount sda1 into /media/Tough/ 
 sudo mount /dev/sda1 /media/Tough/
-# images, no preview, x&y inverted, 1h duration, every 3 sec, 4 digits name, saved as jpg
-raspistill -bm -hf -vf -t 3600000 -tl 3000 -o /media/Tough/lapse/image%04d.jpg
+# WD will want this one instead>>
+sudo ntfs-3g /dev/sda1 /media/Tough/
+sudo chmod 775 /media/Tough
+sudo chmod 775 /media/Tough/lapse
+# images, no preview, x&y inverted, 1080p, 1h duration, every 3 sec, 4 digits name, saved as jpg
+raspistill -bm -hf -vf -w 1920 -h 1080 -t 3600000 -tl 3000 -o /media/Tough/lapse/image%04d.jpg
 # unmount sda1
+sleep 1m
 sudo umount /dev/sda1
